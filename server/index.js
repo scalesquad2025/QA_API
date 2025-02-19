915478dd97617866bbb61c9bf7cd8580474fb6d0
tranbonnie <tran.bonniee@gmail.com>
2025-02-19 15:05:20 -0800

    removed auth

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const { Client } = require('pg');
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const auth = require('../../Atelier/server/middleware/authorization.js');
 const axios = require('axios');
 
 const app = express();
