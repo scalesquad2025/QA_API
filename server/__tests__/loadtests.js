ad205ec01ffc10fb6bf2c55cd696bd0f4f465cf9
tranbonnie <tran.bonniee@gmail.com>
2025-04-09 13:24:28 -0700

    .

--- server/__tests__/loadtests.js
+++ server/__tests__/loadtests.js
@@ -1,34 +1,16 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
-require('dotenv').config();
 
 export const options = {
   stages: [
-    // {
-    //   duration: '60s',
-    //   target: 1
-    // }
-    // {
-    //   duration: '60s',
-    //   target: 10
-    // }
     {
       duration: '60s',
       target: 100
     }
   ]
-  // 1 minute 1000
-  // 1 minute 0
-
-  // cloud: {
-  //   // Project: k6
-  //   projectID: 3748550,
-  //   // Test runs with the same name groups test runs together.
-  //   name: 'Test (18/02/2025-19:16:11)'
-  // }
 };
 
 export default function() {
-  http.get(`http://${process.env.HOST}:${process.env.PORT}/api/qa/questions?product_id=1`);
+  http.get(`http://18.119.167.80:3000/api/qa/questions?product_id=1`);
   sleep(1);
 }
