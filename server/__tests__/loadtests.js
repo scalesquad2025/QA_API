5015f2a3d8acd7e99c6de9796754e2ea300851a5
tranbonnie <tran.bonniee@gmail.com>
2025-04-09 13:52:45 -0700

    .

--- server/__tests__/loadtests.js
+++ server/__tests__/loadtests.js
@@ -5,12 +5,14 @@ export const options = {
   stages: [
     {
       duration: '60s',
-      target: 100
+      target: 10
     }
   ]
 };
 
 export default function() {
-  http.get(`http://18.119.167.80:3000/api/qa/questions?product_id=1`);
+  http.get(`http://18.119.167.80:3000/api/qa/questions?product_id=1`, {
+    timeout: '180s'
+  });
   sleep(1);
 }
